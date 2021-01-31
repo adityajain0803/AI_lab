8bf823bb920bc988dc26a7d52631997298ef1e1a
adityajain0803 <55041154+adityajain0803@users.noreply.github.com>
2021-02-01 00:02:57 +0530

    Update and rename Camel.py to Path.py

---
+++ Path.py
@@ -0,0 +1,29 @@
+x = y = 0
+
+s = {(0, 0)}
+
+for p in path:
+
+	if p == "N":
+
+		y += 1
+
+	elif p == "S":
+
+		y -= 1
+
+	elif p == "W":
+
+		x += 1
+
+	else:
+
+		x -= 1
+
+	if (x, y) in s:
+
+		return True
+
+	s.add((x, y))
+
+return False
